4819e8c8ec8256aea22f000406fd2ffe9c844c71
Kevin Paul Montealegre Melo <42593542+Paul9834@users.noreply.github.com>
2019-09-06 18:48:42 -0500

    Paul9834
    initial commit

---
+++ Prueba/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Prueba
+//
+//  Created by Estudiante Politecnico Grancolombiano on 9/6/19.
+//  Copyright © 2019 Estudiante Politecnico Grancolombiano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+    
+
+}
+
